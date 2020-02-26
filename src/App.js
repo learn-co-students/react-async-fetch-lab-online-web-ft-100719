e5fe6f72d07251ba8c3ca6ce50fa6fa22c63acf7
yasmine-hj <yasminehajaji95@gmail.com>
2020-02-26 11:01:24 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,35 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export class App extends Component {
+  state = {
+    peopleInSpace: []
+  }
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Could not fetch pets');
+      }
+    })
+    
+    .then(data => {
+      this.setState({
+        peopleInSpace: data.people
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.peopleInSpace.map((person, id) => <h3 key={id}> {person.name} </h3> ) }
+      </div>
+    )
+  }
+}
+
+export default App
