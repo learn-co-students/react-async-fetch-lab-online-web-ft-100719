b8ceef806b683094f2b752fc78705141fcf7052e
hfarnsworth <havilahfarnsworth@gmail.com>
2020-02-25 23:52:23 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
 // create your App component here
+import React from 'react';
+
+export default class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(r=>r.json())
+    .then(console.log);
+  };
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
